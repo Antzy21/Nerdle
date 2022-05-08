00a3762d81d72a31b1b4b3ff2fddf3defe044299
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-05-08 19:33:45 +0100

    Add examples

--- nerdleSolver.py
+++ nerdleSolver.py
@@ -1,18 +1,21 @@
 from solvingHelpers import isSensible, isCorrect, produceVariations
 
-func = "xxxxx"
+func = "xxxxxx"
+#Example [3,6,5,'+']
 mustUse = []
 cantUse = []
+#Example [(0, '9'), (7, '5')]
 positionalConditions = [
 ]
 
 variations = produceVariations(mustUse, cantUse, func)
 print(len(variations), "variations")
-       
+#print(variations[0:100])       
+
 sensibleEquations = list(filter(isSensible, variations))
 print(len(sensibleEquations), "sensible equations")
+#print(sensibleEquations[0:100])       
 
 correctEquations = list(filter(isCorrect, sensibleEquations))
 print(len(correctEquations), "correct equations")
-
 print(correctEquations)        
